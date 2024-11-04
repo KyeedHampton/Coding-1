52ec0f74564afb1c7754a3bcd10e413b354058c4
KyeedHampton <khampton324@boyslatin.net>
2024-11-04 13:57:04 +0000

    finished the async for Nov 4

---
+++ ClassNotes/pythonDataTypes.py
@@ -0,0 +1,28 @@
+# string data types 
+# String are pieces of data wrapped in quotation marks.
+                               
+                               
+# Integers 
+# integers are pieces of data that represent whole numbers. 
+# whole numbers do not have decimals 
+ 
+ 
+# Floats 
+# floats represent pieces of data that contain a decimal 
+ 
+ 
+# Booleans 
+# Booleans are pieces of data that represent true or false values 
+ 
+# Print 
+ # A function that prints the value you provide to it 
+ 
+ # Input
+  #  A function that takes in that value you provide to it 
+  
+  # variables 
+  # variables are containers for data that can be re-used
+ 
+{},
+[],
+(),
---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,36 @@
+# data types = are the fundamental builing blocks of Python programming
+#  strings = any characters wrapped in quotation marks.
+# boolean = true and false 
+# integers = whole numbers
+687
+
+# floats = decimal numbers 
+687.223
+
+# variables = containers for data 
+# we create varibales using the following steps :
+# 1. create a variable name
+# 2. apply an equal sign 
+# 3. give it a data type value 
+
+user ="Kyeed,"
+message =' Good Morning '
+weather = "It is 60 degress and sunny."
+
+#we use print to see data in our terminal.
+print(message + user + " " + weather)
+
+number1= 1200
+number2= 800
+
+print(number1 + number2)
+
+() #round brackets = function
+{} #curly brackets = objects
+[] #square3 brackets = lists 
+
+message = 'Yo'
+name = 'my name is Kyeed, '
+activity ='i am going to sleep.'
+time = ' I will sleep at 8:15.'
+print(message + " " + name + activity + time)
