5490a8a36b1af0b5a084b43836d58643bead7852
KyeedHampton <khampton324@boyslatin.net>
2024-11-13 14:35:39 +0000

    worked on operators for py

---
+++ Unit3_python/AsyncNov4.py
@@ -0,0 +1,17 @@
+# Creating two variables
+x = 200
+y = 45
+# Adding and printing the result
+print(x + y) # Output: 245
+
+# I would use a list for the days of the week (like ["Monday", "Tuesday", ...]).
+# I would use an integer data type for the date (like 1, 2, 3, etc.).
+
+# A float is a number with a decimal (like 3.14), while an integer is a whole number (like 5).
+
+# A comment section on a picture on Instagram would be a string (like "Nice pic!").
+# A survey question asking if someone has traveled would be a Boolean (like True or False).
+
+# 1. More real-life examples to practice code. 
+# 2. Extra time to ask questions at the end of class.
+# 3. Feedback on my code to know where I can improve.
