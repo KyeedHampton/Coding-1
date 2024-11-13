e2cd1e1651ee07c8e9d4face612865ec5aa11fb0
KyeedHampton <khampton324@boyslatin.net>
2024-11-13 14:35:58 +0000

    worked on operator for py

--- Unit3_python/AsyncNov4.py
+++ Unit3_python/AsyncNov4.py
@@ -2,14 +2,16 @@
 x = 200
 y = 45
 # Adding and printing the result
-print(x + y) # Output: 245
+print(x + y) 
 
 # I would use a list for the days of the week (like ["Monday", "Tuesday", ...]).
+
 # I would use an integer data type for the date (like 1, 2, 3, etc.).
 
 # A float is a number with a decimal (like 3.14), while an integer is a whole number (like 5).
 
-# A comment section on a picture on Instagram would be a string (like "Nice pic!").
+# A comment section on a picture on Instagram would be a string (like "nice pic!").
+
 # A survey question asking if someone has traveled would be a Boolean (like True or False).
 
 # 1. More real-life examples to practice code. 
---
+++ Unit3_python/operatorReview.py
@@ -0,0 +1,36 @@
+# Arithmetic operator - writing code that does math processes
+depositAmount = 50
+witdrawAmount = 25
+checkingAccount = 1000
+savingAccount = 2000
+creditAmount = 1500
+
+print(depositAmount + checkingAccount)
+
+
+# Assignment operator- giving variables values. KEYWORD is assigrment 
+#any time you are using a variables , you are using the assignment operator
+teeshirt=50.00
+
+discount_25= .25
+
+appliedDiscount = teeshirt * discount_25
+total= teeshirt - appliedDiscount
+
+print(total)
+
+# Comparision operator- analyzing two values to determine an outcome.
+
+wendysReviewScore= 4.0
+burgerKinbgReviewScore = 4.2
+
+print(burgerKinbgReviewScore > wendysReviewScore )
+print(burgerKinbgReviewScore < wendysReviewScore )
+
+dailyCaloricINtake = 3000
+
+ApplesbeeMeal = 2300
+
+# compsre best 
+
+# Logical operator
